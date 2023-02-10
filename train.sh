e752d4f11dd1e93b17bbc1a25ddfecf3bdcfa5fc
Richard <rcorrero@stanford.edu>
2023-02-10 11:07:38 -0600

    Add train script

--- train.sh
+++ train.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 CONDAENV=zulu
-conda run -n $CONDAENV python -m train
+
+read -p "Path to file containing command: " CMDFILE
+read -p "Path to file containing seeds: " SEEDFILE
+
+echo "Command filepath: $CMDFILE"
+echo "Seed filepath: $SEEDFILE"
+
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+while read line; do    
+    echo "Seed: $line"    
+done < $SEEDFILE
+# conda run -n $CONDAENV python -m train
