1fed63f6fbac338145173ca02e7d06faebb991c2
Richard <rcorrero@stanford.edu>
2023-02-10 12:37:26 -0600

    Remove input

--- train.sh
+++ train.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-read -p "Conda environment name: " CONDAENV
-read -p "Path to file containing command: " CMDFILE
-read -p "Path to file containing seeds: " SEEDFILE
+CONDAENV=$1
+CMDFILE=$2
+SEEDFILE=$3
+
+# read -p "Conda environment name: " CONDAENV
+# read -p "Path to file containing command: " CMDFILE
+# read -p "Path to file containing seeds: " SEEDFILE
 
 echo "Conda environment name: $CONDAENV"
 echo "Command filepath: $CMDFILE"
 echo "Seed filepath: $SEEDFILE"
 
-read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+# read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 while read seed; do  
     echo "Seed: $seed"
     while read cmd; do
         echo "Command: $cmd"
-        conda run -n $CONDAENV nohup python -m train $cmd --seed $seed > _auto.out & 
-        PID=$!
-        wait $PID
+        conda run -n $CONDAENV python -m train $cmd --seed $seed
+        # PID=$!
+        # wait $PID
     done < $CMDFILE
 done < $SEEDFILE
